f2785db1b6945cf6ead54710d9ffc4071e8c574e
ryo322 <135493789+ryo322@users.noreply.github.com>
2023-09-25 07:51:39 +0000

    [add]閲覧数

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :user_rooms, dependent: :destroy
   has_many :chats, dependent: :destroy
   
+  has_many :view_counts, dependent: :destroy
+
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
   
