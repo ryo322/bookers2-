d64331e760033e52197f8b68355f18802467619f
ryo322 <135493789+ryo322@users.noreply.github.com>
2023-10-01 00:32:42 +0000

    [add[評価機能

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    jquery: 'jquery/src/jquery',
+  })
+)
+
 module.exports = environment
 
 const webpack = require('webpack')
